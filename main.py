d570891d2d8306c8144bbbd82c1e3ee109087201
AliyuBello-CS <hydaraliy14@gmail.com>
2026-09-17 20:38:01 -0400

    Sigmoid, Loss

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ Y = np.array([0,0,1,1])
 print (X.shape)
 print (Y.shape)
 
+# Calculate scores using weights and bias.
+# Convert scores into probabilities using sigmoid.
+# Compare those probabilities with the actual outcomes to calculate loss.
+# During training, adjust weights and bias to reduce that loss.
 
 # EX1
 
@@ -40,7 +44,7 @@ else:
 print("Prediction:", prediction)
 
 
-# EX2
+# EX3
 weights = np.array([0.5, 0.1])
 bias = -3
 
@@ -55,3 +59,30 @@ print(probabilities)
 predictions = (probabilities >= 0.5).astype(int)
 
 print(predictions)
+
+# EX4= PROBA AND PRED loss
+
+
+probability = 0.8
+loss = -np.log(probability)
+print (loss)
+
+
+probability = 0.2
+loss = -np.log(1 - probability)
+
+print(loss)
+
+
+# CROSS ENTROPY
+
+y = np.array([0, 0, 1, 1])
+
+losses = -(y * np.log(probabilities)
+           + (1 - y) * np.log(1 - probabilities))
+
+print("Each student's loss:", losses)
+
+loss = np.mean(losses)
+
+print("Average loss:", loss)
